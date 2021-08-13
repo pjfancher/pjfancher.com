dfcf5f18fa452574823d569cce012086026d15d2
PJ Fancher <pj@digitalbrands.com>
2021-08-13 15:36:54 -0400

    next@unknown: Added front and back apps

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f7da24ebcf15303fd3bc3be8198cf142'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'de5cfb8a-a5f2-4c4b-b458-536965bbe4e7'
+};
